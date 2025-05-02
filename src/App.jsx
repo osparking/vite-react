d5ce9c823ccda5fdbda64e66c2c969a580a94ed1
박종범 <jbpark03@gmail.com>
2025-05-02 21:04:47 +0900

    camel onClick, style constant

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css';
 
 function App() {
   const zipCode = "17274";
+  const buttonStyle = {
+    backgroundColor: "purple", color:
+      "ivory"
+  }
   return (
     <>
       <p>우편번호: {zipCode}</p>
-      <button style={{
-        backgroundColor: "purple", color:
-          "ivory"
-      }}>클릭해!</button>
+      <button style={buttonStyle}>클릭해!</button>
+      <p>우편번호: {zipCode}</p>
+      <button
+        style={buttonStyle}
+        onClick={() => alert('참 잘했어요')}>클릭해!</button>
       <p className='card'>여러분 모두...</p>
     </>
   )
