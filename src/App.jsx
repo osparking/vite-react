6000e50c7a4b00058c89e79378011f64b13f8e38
박종범 <jbpark03@gmail.com>
2025-05-02 20:51:26 +0900

    curly braces, camel case className

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css';
 
 function App() {
-  const greet = <h1>안녕?</h1>;
-  return (
-    <>
-      { greet }
-      {2000 + 25}년
+const zipCode = "17274";
+return (
+  <>
+    <p>우편번호: {zipCode}</p>
       <p className='card'>여러분 모두...</p>
     </>
   )
