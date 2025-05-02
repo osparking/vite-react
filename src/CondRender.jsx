6bd0a9f3fdaf2176232428985abcd802c42b77eb
박종범 <jbpark03@gmail.com>
2025-05-03 07:56:46 +0900

    toggle component rendering based on bool flag

--- src/CondRender.jsx
+++ src/CondRender.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 
 const CondRender = () => {
-  const isLoggedIn = false;
-return (
-  <div>
-    {isLoggedIn ?
-      "어서오세요." : "로그인해요."
-    }
-  </div>
-)
+  const alertErrorMsg = true;
+  const errorMsg = "입력한 자격 정보에 오류가 있습니다."
+  return (
+    <div>
+      {alertErrorMsg && <p>{errorMsg}</p>
+      }
+    </div>
+  )
 }
 
 export default CondRender
