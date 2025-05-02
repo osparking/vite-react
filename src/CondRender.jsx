efc8c3f0bc9e06719687399a341dd733bf25dd56
박종범 <jbpark03@gmail.com>
2025-05-03 06:35:58 +0900

    ternary operator used for condi' render' - 2

--- src/CondRender.jsx
+++ src/CondRender.jsx
@@ -3,13 +3,13 @@ import './App.css';
 
 const CondRender = () => {
   const isLoggedIn = false;
-  return (
-    <div>
-      {isLoggedIn ?
-        <div>어서오세요</div> : <div>로그인해요</div>
-      }
-    </div>
-  )
+return (
+  <div>
+    {isLoggedIn ?
+      "어서오세요." : "로그인해요."
+    }
+  </div>
+)
 }
 
 export default CondRender
