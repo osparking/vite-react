919c9892924ba2d8399ad9a4de2d34afeb59d8c4
박종범 <jbpark03@gmail.com>
2025-05-02 20:58:01 +0900

    camel case attribute name backgroundColor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
-const zipCode = "17274";
-return (
-  <>
-    <p>우편번호: {zipCode}</p>
+  const zipCode = "17274";
+  return (
+    <>
+      <p>우편번호: {zipCode}</p>
+      <button style={{
+        backgroundColor: "purple", color:
+          "ivory"
+      }}>클릭해!</button>
       <p className='card'>여러분 모두...</p>
     </>
   )
