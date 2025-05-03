c725976fb1546303b19bb6ce3c370500a191fb7f
박종범 <jbpark03@gmail.com>
2025-05-03 14:58:53 +0900

    short intro on myself

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,12 @@ function App() {
           <a href="#contact">연락</a>
         </nav>
       </header>
+      {/* 소개 구획 */}
+      <section id='about' className='about-section'>
+        <h2>자기 소개</h2>
+        <p>안녕하세요? 제 이름은 {name}이고, 저는 직업 군인이었으나 
+          전역 후 자바-리액트 웹앱 개발에 진심인 남성입니다.</p>
+      </section>
     </div>
   );
 }
