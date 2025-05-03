d537d5c243842a5e224b3cda9c5bf7ae00ab982a
박종범 <jbpark03@gmail.com>
2025-05-03 11:24:00 +0900

    check time say message one of two

--- src/UsingFunc.jsx
+++ src/UsingFunc.jsx
@@ -3,13 +3,22 @@ import React from 'react'
 const printLastname = function (lastname) {
   return <p>당신의 성씨는: {lastname}</p>
 }
+
+const greetCheckingTime = (isMorning) => {
+  let msg = "즐거운 하루";
+  if (isMorning) {
+    msg ="좋은 아침~"
+  }
+  return msg;
+}
+
 const UsingFunc = () => {
   const names = ['김', '이', '박'];
+  const isMorning = (new Date()).getHours() < 12;
 
   return (
     <div>
-      {names.map((name) => printLastname(name)
-      )}
+      <p>{greetCheckingTime(isMorning)}</p>
     </div>
   )
 }
