ff1dfd23090bcd4a2b0998095c14e0d54721a63d
박종범 <jbpark03@gmail.com>
2025-05-03 14:07:05 +0900

    adds two more personal project

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,16 @@ function App() {
       title: "오픈 소스 파킹(Open Source Parking)",
       description: "주차관제 제어체계 오픈 소스 프로젝트",
       link: "https://github.com/osparking/OsParking_src"
+    },
+    {
+      title: "애완동물 병원 웹 앱 - 전단",
+      description: "자바, 리액트, 마리아DB 사용 개발 프로젝트",
+      link: "https://github.com/osparking/pet-care-front-react-java"
+    },
+    {
+      title: "애완동물 병원 웹 앱 - 후단",
+      description: "자바, 리액트, 마리아DB 사용 개발 프로젝트",
+      link: "https://github.com/osparking/pet-care-react"
     }
   ]
 
