617ad6eedbf4fea15d18d9a702ed169d2440290d
박종범 <jbpark03@gmail.com>
2025-05-04 09:09:03 +0900

    contact info compo'

---
+++ src/component/Contact.jsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (<section id='contact' className='contact-section'>
+    <h2>연락처</h2>
+    <p>저에게 연락하려면 이메일을 주십시오.
+      <a href="mailto:jbpark03@gmail.com">jbpark03@gmail.com</a>.</p>
+  </section>);
+}
+
+export default Contact;
