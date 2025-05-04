e2c4c4376b3167cdec6df5c63f60790072c683d3
박종범 <jbpark03@gmail.com>
2025-05-04 09:17:13 +0900

    remove not really useful comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,11 @@ import ProjectList from './component/ProjectList';
 function App() {
   return (
     <div className='App'>
-      {/* 헤더 구획 */}
       <Header />
-
-      {/* 소개 구획 */}
       <About />
-
-      {/* 프로젝트 구획 */}
       <ProjectList />
-
-      {/* 연락처 구획 */}
       <Contact />
-
-      {/* 바닥글 구획 */}
       <Footer />
-
     </div>
   );
 }
