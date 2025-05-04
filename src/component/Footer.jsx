640b4fd58a29422172892aaa9d311a28535b9cef
박종범 <jbpark03@gmail.com>
2025-05-04 09:10:03 +0900

    footer area compo'

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>2025. 모든 권리는 저자가 보유함.</p>
+    </footer>
+  )
+}
+
+export default Footer;
